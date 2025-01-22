ebb35e4300c79db1495285f77e511599b495fe2f
Niel Swusten <r0938850@student.thomasmore.be>
2025-01-22 13:03:38 +0100

    aanpassinge

--- photo-gallery/src/app/tab2/tab2.page.ts
+++ photo-gallery/src/app/tab2/tab2.page.ts
@@ -9,10 +9,29 @@ import { PhotoService } from '../services/photo.service';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService) {}
+  constructor(public photoService: PhotoService) {
+    // Example: Calculate angle to target (300, 300) from center of screen
+    const startPoint = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+    const targetPoint = { x: 300, y: 300 };
+
+    this.angle = this.calculateAngle(startPoint, targetPoint);
+
+  }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
 
+  angle: number = 0;
+
+
+
+  calculateAngle(start: { x: number; y: number }, end: { x: number; y: number }): number {
+    const dx = end.x - start.x;
+    const dy = end.y - start.y;
+    return Math.atan2(dy, dx) * (180 / Math.PI); // Convert radians to degrees
+  }
+
 }
+
+
--- photo-gallery/src/environments/environment.ts
+++ photo-gallery/src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: false,
   // baseUrl: "http://localhost:8000/api",
-  // baseUrl: 'http://10.146.36.142:8000/api',
-  baseUrl: 'http://172.20.10.7:8000/api',
+  baseUrl: 'http://api-lb-1568128161.us-east-1.elb.amazonaws.com:8000/api',
 };
 
 /*
